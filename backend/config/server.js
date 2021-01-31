a2b8b33d115cf84b4193f23e1c2fc4a8e3be0333
Sebastián Sánchez <sebastian.asm@outlook.com>
2021-01-30 21:23:22 -0300

    creacion del backend con strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '836df772d029d51eba12cb6162612634'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b7e0cb6c-d99b-4931-a12e-220241c9d24b'
+};

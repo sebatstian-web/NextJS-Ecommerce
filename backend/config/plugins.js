e879611bed49a288313adf942d58b4d04b6cb1c1
Sebastián Sánchez <sebastian.asm@outlook.com>
2021-02-08 18:23:01 -0300

    configuracion de cloudinary

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,13 +1,24 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'aws-s3',
+    provider: 'cloudinary-folderoptions',
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: 'sa-east-1',
-      params: {
-        Bucket: 'nextjs-ecommerce',
-      },
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+      default_folder: env('CLOUDINARY_DEFAULT_FOLDER'),
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   upload: {
+//     provider: 'aws-s3',
+//     providerOptions: {
+//       accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//       secretAccessKey: env('AWS_ACCESS_SECRET'),
+//       region: 'sa-east-1',
+//       params: {
+//         Bucket: 'nextjs-ecommerce',
+//       },
+//     },
+//   },
+// });
